92c4bd9ea46c7a89b1936c20b0ed6439ab55cbb8
Athos <dracarys1312@gmail.com>
2020-12-27 01:30:27 +0700

    HomePage layout and style #2 #3 #4 #5 #6 #7

--- src/App.js
+++ src/App.js
@@ -5,24 +5,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './App.css';
 
-function App() {
-  return (
-    <div classNameName="App">
-      <nav className="navbar navbar-expand-md navbar-dark my-bg-primary">
-        <div className="container"> <button className="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbar">
-            <ul className="navbar-nav mx-auto"></ul>
-            <ul className="navbar-nav">
-              <li className="nav-item"> <a className="nav-link text-white" href="#">Home</a> </li>
-              <li className="nav-item"> <a className="nav-link text-white" href="#">Instructions</a> </li>
-            </ul>
+import HomePage from './components/hompage.component';
+
+class App extends Component {
+  render(){
+      return (
+        <div className="App">
+          <nav className="navbar navbar-expand-md navbar-dark my-bg-primary">
+            <div className="container"> <button className="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbar">
+                <ul className="navbar-nav mx-auto"></ul>
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                  <Link to={"/"} className="nav-link text-white">
+                  Home
+                  </Link>
+                  </li>
+                  <li className="nav-item">
+                  <Link className="nav-link text-white" to={"/"}>
+                  Instructions
+                  </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          <div className="container-fluid">
+            <Switch>
+              <Route exact path={["/"]} component={HomePage} />
+            </Switch>
           </div>
         </div>
-      </nav>
-    </div>
-  );
-}
-
+      );
+    }
+  }
 export default App;
---
+++ src/components/hompage.component.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="container-fluid h-100">
+          <div className="row align-items-center h-100">
+            <div className="order-2 order-md-1 col-md-6 px-5 pt-5">
+              <h3 className="bold text-center">Enter a name</h3>
+              <form className="form">
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="name"/>
+                </div>
+              </form>
+              <div className="divider div-transparent div-dot"></div>
+              <div className="d-flex justify-content-center">
+                <button className="btn" type="button">Upload a CSV file</button>
+              </div>
+              <h5 className="text-center my-3"> Don't know how to generate form? <a className="link-instructions" href="#">See our instruction</a></h5>
+            </div>
+            <div className="h-75 order-2 order-md-1 col-md-6">
+              <div className="box-fileupload">
+                <input type="file" id="fileId" className="file-upload-input" name="files" multiple=""/>
+                <label for="fileId" className="file-upload-btn"></label>
+                <p className="box-fileupload__lable">Upload a certificate template</p>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex justify-content-end">
+            <button className="btn" type="button">Generate</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
