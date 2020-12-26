6212af71bef542295675de4ea97c19435e1779f7
Athos <dracarys1312@gmail.com>
2020-12-27 01:55:37 +0700

    Change some spacings

--- src/components/hompage.component.js
+++ src/components/hompage.component.js
@@ -9,7 +9,7 @@ export default class HomePage extends Component {
           <div className="row align-items-center h-100">
             <div className="order-2 order-md-1 col-md-6 px-5 pt-5">
               <h3 className="bold text-center">Enter a name</h3>
-              <form className="form">
+              <form className="form pt-3">
                 <div className="input-group">
                   <input type="text" className="form-control" placeholder="name"/>
                 </div>
@@ -21,7 +21,7 @@ export default class HomePage extends Component {
                   <input type="file" name="myfile" />
                 </div>
               </div>
-              <h5 className="text-center my-3"> Don't know how to generate form? <a className="link-instructions" href="#">See our instruction</a></h5>
+              <h5 className="text-center my-5"> Don't know how to generate form? <a className="link-instructions" href="#">See our instruction</a></h5>
             </div>
             <div className="h-75 order-2 order-md-1 col-md-6">
               <div className="box-fileupload">
