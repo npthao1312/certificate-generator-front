c76a196e3e19ce9074cea69361dd78d934098d3f
Athos <dracarys1312@gmail.com>
2020-12-26 21:49:30 +0700

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <nav className="navbar navbar-expand-md navbar-dark my-bg-primary">
+        <div className="container"> <button className="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbar">
+            <ul className="navbar-nav mx-auto"></ul>
+            <ul className="navbar-nav">
+              <li className="nav-item"> <a className="nav-link text-white" href="#">Home</a> </li>
+              <li className="nav-item"> <a className="nav-link text-white" href="#">Instructions</a> </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
