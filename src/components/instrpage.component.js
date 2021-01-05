0f2b9d307f8d0b4a08af04e3bf0bddb01cdf3c07
Athos <dracarys1312@gmail.com>
2021-01-05 16:33:49 +0700

    Instruction Page #11 #12

--- src/components/instrpage.component.js
+++ src/components/instrpage.component.js
@@ -4,14 +4,28 @@ import { Link } from "react-router-dom";
 export default class InstrPage extends Component {
   render() {
     return (
-      <div className="main">
-        <div className="container-fluid h-100">
-          <div className="row align-items-center h-100">
-            1. Upload a certificate template and an excel file including names.<br/>
-            2. Click generate to have a list of certificates with respective names
-          </div>
+  <div className="py-3">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12"><img className="img-fluid d-block center-block mx-auto" src="https://media.gettyimages.com/videos/modern-question-mark-line-icon-animation-on-white-background-video-id1213673269?s=640x640"/></div>
+      </div>
+    </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Introduction</h1>
+          <p> After two classes, we found that we always had to put names of students into certificate templates respectively manually. That's why we come up with a solution to automate this process to reduce the time taken. </p>
+          <h1> Instructions </h1>
+          <ol className="">
+            <li>Upload a certificate template and an excel file including names.</li>
+            <li>Position text for name on the certificate image template.</li>
+            <li>Click generate to have a list of certificates with respective names.</li>
+            <li>Download</li>
+          </ol>
         </div>
       </div>
+    </div>
+  </div>
     );
   }
 }
